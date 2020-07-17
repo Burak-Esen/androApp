7e64e410a64de170f440be5ce4267853dd4a212e
Burak-Esen <0.burak.esen@gmail.com>
2020-07-17 16:39:57 +0300

    Hello toast--second activity

--- googleDevAnd/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ googleDevAnd/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,9 +10,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE="com.example.android.helloworld.extra.MESSAGE";
+    public static final int RESET_REQUEST=1;
     private int count=0;
     private TextView showCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +24,33 @@ public class MainActivity extends AppCompatActivity {
         showCount=(TextView) findViewById(R.id.text_show_count);
     }
 
-    public void resetCount(View view){
-        Toast toast=Toast.makeText(this,R.string.reset_counter_message, Toast.LENGTH_SHORT);
-        this.count=0;
-        if (showCount != null)
-            showCount.setText(Integer.toString(count));
-        toast.show();
+    public void lunchHelloActivity(View view){
+//        Toast toast=Toast.makeText(this,R.string.reset_counter_message, Toast.LENGTH_SHORT);
+//        this.count=0;
+//        if (showCount != null)
+//            showCount.setText(Integer.toString(count));
+//        toast.show();
+        Intent intent = new Intent(this, HelloActivity.class);
+        String number = String.valueOf(count);
+        intent.putExtra(EXTRA_MESSAGE, number);
+        startActivityForResult(intent, RESET_REQUEST);
     }
 
     public void countUp(View view){
         this.count++;
         if (showCount != null)
             showCount.setText(Integer.toString(count));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESET_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                int reset = data.getIntExtra(HelloActivity.EXTRA_RESET, 0);
+                count=reset;
+                showCount.setText("0");
+            }
+        }
     }
 }
